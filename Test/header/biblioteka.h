77ef90e47e1d0311259af094ffc3d7594c55740e
hirochima2 <99726382+hirochima2@users.noreply.github.com>
2022-12-15 18:03:12 +0100

    Ubacio sam kakva će biti struktura programa
    Evo da imate po čemu da se vodite kakva će odprilike bit strukuta

---
+++ Test/header/biblioteka.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+void print2();
+int print6();
+void sve();
+class Biblioteka
+{
+    public:
+        void print();
+        int print3();
+};
+
